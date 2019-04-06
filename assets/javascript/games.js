f1535f4849f0b95abee5f3bbc00ac0e68350d49e
EBayler <erika.bayler@gmail.com>
2019-04-06 11:14:46 -0600

    updated

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -4,6 +4,7 @@
 //The random number shown at the start of the game should be between 19 - 120 [Math.floor(Math.random() * 120) + 19;].
 // when clicking any crystal it needs to add to our current number index until it either matches or exceeds the target number (if/else statement)
 // if it is equal we need to increment wins++ counter or decrement losses-- if it exceeds
+
 $(document).ready(function () { //
     var randomResult = 0;
     var wins = 0;
@@ -12,6 +13,15 @@ $(document).ready(function () { //
 
 
     var letsDoThis = function () {
+        $(".crystals").empty();
+
+        var crystalImages = [
+            "./assets/images/crystal1.png",
+            "./assets/images/crystal2.png",
+            "./assets/images/crystal3.png",
+            "./assets/images/crystal4.png",
+        ];
+
         randomResult = Math.floor(Math.random() * 101) + 19;
         $("#challengeNumber").html('Match this #: ' + randomResult)
 
@@ -25,22 +35,38 @@ $(document).ready(function () { //
                 "class": 'crystal',
                 "dataRandom": random
             });
+            crystal.css({
+                "background-image": "url(" + crystalImages[i] + ")",
+                "background-size":"cover"
+            })
             $("#crystals").append(crystal);
         }
+        $("#runningTotal").html("Your total score is: " + addEmUp);
     }
+
     letsDoThis();
 
-    $(".crystal").on('click', function () {
+    $(document).on('click', ".crystal", function () {
         // console.log(this)
         var number = parseInt($(this).attr('dataRandom'));
         addEmUp += number;
         // console.log(addEmUp)
+        $("#runningTotal").html("Your total score is: " + addEmUp);
+
         if (addEmUp === randomResult) {
             wins++;
-            $("#wins").html(wins);
+            $("#wins").html("Wins: " + wins);
+            addEmUp = 0;
+
+            letsDoThis();
+
+
         } else if (addEmUp > randomResult) {
-            losses--;
-            $("#losses").html(losses);
+            losses++ ;
+            $("#losses").html(" Losses: " + losses);
+            addEmUp = 0;
+
+            letsDoThis();
 
         }
     });
